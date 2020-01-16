00beac6915e1eca1a4dbb8fbea267976187b5825
klowrey <kendall.lowrey@gmail.com>
2020-01-16 15:43:25 -0800

    added _site util function which unpacks sites into an SVector

--- src/jlmujoco.jl
+++ src/jlmujoco.jl
@@ -4,3 +4,6 @@ jl_id2name(m::jlModel, objtype, id::Integer) = MJCore.mj_id2name(m, objtype, Int
 jl_enabled(m::jlModel, flag::MJCore.mjtEnableBit) = _flagtest(m.opt.enableflags, flag)
 jl_disabled(m::jlModel, flag::Union{Integer, MJCore.mjtDisableBit}) = _flagtest(m.opt.disableflags, flag)
 _flagtest(flags, flag) = Integer(flags) & Integer(flag) == Integer(flag)
+
+@propagate_inbounds _site(site)::SVector{3,Float64} = SA_F64[site[1], site[2], site[3]]
+@propagate_inbounds _site(sites, id)::SVector{3,Float64} = SA_F64[sites[1,id], sites[2,id], sites[3,id]]
